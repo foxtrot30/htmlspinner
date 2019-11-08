d1546e4ee6c99ade95b6941f17ad615732e4071a
Fathalfath30 <fathalfath30@gmail.com>
2019-11-08 09:19:30 +0700

    first init

---
+++ app/src/main/java/com/foxtrot30/htmlspinnerlib/Example.java
@@ -0,0 +1,13 @@
+package com.foxtrot30.htmlspinnerlib;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Example extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
